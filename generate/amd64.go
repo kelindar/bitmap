8e342e7d347e841b55fab711cda3dab2cfc40888
Roman Atachiants <roman.atachiants@gmail.com>
2021-06-25 13:51:19 +0400

    Unrolled popcount

--- generate/amd64.go
+++ generate/amd64.go
@@ -5,7 +5,7 @@
 
 package main
 
-//go:generate go run asm.go -out ../bitmap_amd64.s -stubs ../stub_amd64.go -pkg=bitmap
+//go:generate go run amd64.go -out ../bitmap_amd64.s -stubs ../stub_amd64.go -pkg=bitmap
 
 import (
 	"strings"
@@ -20,15 +20,88 @@ func main() {
 	Constraint(buildtags.Not("appengine").ToConstraint())
 	Constraint(buildtags.Not("noasm").ToConstraint())
 	Constraint(buildtags.Term("gc").ToConstraint())
-	generate("and")
-	generate("andnot")
-	generate("or")
-	generate("xor")
+
+	// Generate count
+	// Useful examples: https://github.com/WojciechMula/sse-popcount
+	// TODO: explore Harley-Seal with AVX2/512 https://arxiv.org/pdf/1611.07612.pdf
+	makeCount()
+
+	// Generate boolean algebra
+	makeOp("and")
+	makeOp("andnot")
+	makeOp("or")
+	makeOp("xor")
+
 	Generate()
 }
 
-// generate generates an SIMD "and", "or" , "andnot"operations.
-func generate(op string) {
+// makeCount generates a faster way of counting ones using popcount
+func makeCount() {
+	TEXT("x64count", NOSPLIT, "func(a []uint64) uint64")
+	Doc("x64count counts the bits set to one")
+
+	// Load the array and its length
+	Pragma("noescape")
+	a := Mem{Base: Load(Param("a").Base(), GP64())}
+	n := Load(Param("a").Len(), GP64())
+
+	// The register for the sum, reset to zero
+	sum := GP64()
+	XORQ(sum, sum)
+
+	const size, unroll = 8, 4 // bytes (64bit * 4)
+	const blocksize = size * unroll
+
+	// Create a vector
+	var vector []GPVirtual
+	for i := 0; i < unroll; i++ {
+		vector = append(vector, GP64())
+	}
+
+	Commentf("perform vectorized operation for every block of %v bits", blocksize*8)
+	Label("body")
+	CMPQ(n, U32(1*unroll))
+	JL(LabelRef("tail"))
+
+	// Do the unrolled counting
+	Commentf("count the bits, 4 numbers at a time")
+	for i := 0; i < unroll; i++ {
+		POPCNTQ(a.Offset(size*i), vector[i])
+	}
+
+	// Sum it up
+	for i := 0; i < unroll; i++ {
+		ADDQ(vector[i], sum)
+	}
+
+	// Continue the iteration
+	Comment("continue the interation by moving read pointers")
+	ADDQ(U32(blocksize), a.Base)
+	SUBQ(U32(1*unroll), n)
+	JMP(LabelRef("body"))
+
+	// Now, we only have less than 512 bits left, use normal scalar operation
+	Label("tail")
+	CMPQ(n, Imm(0))
+	JE(LabelRef("done"))
+
+	POPCNTQ(Mem{Base: a.Base}, R15)
+	ADDQ(R15, sum)
+
+	// Continue the iteration
+	Comment("continue the interation by moving read pointers")
+	ADDQ(U32(8), a.Base)
+	SUBQ(U32(1), n)
+	JMP(LabelRef("tail"))
+
+	// Return the sum
+	Label("done")
+	Store(sum, ReturnIndex(0))
+	RET()
+}
+
+// makeOp generates an SIMD "and", "or" , "andnot", "xor" operations.
+func makeOp(op string) {
 	switch op {
 	case "and":
 		TEXT("x64and", NOSPLIT, "func(a []uint64, b []uint64)")
