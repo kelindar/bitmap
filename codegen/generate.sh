2577bbfd4c5f5cbeb41cf7faf7b31feb17d0151a
Roman <roman.atachiants@gmail.com>
2023-05-03 09:30:44 +0400

    use improved gocc

--- codegen/generate.sh
+++ codegen/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./bin/gocc simd_avx.c --arch amd64 -O1 -mavx2 -mfma -masm=intel -o ../
-./bin/gocc simd_neon.c --arch arm64 -O3 -o ../
-./bin/gocc simd_apple.c --arch arm64 -O3 -o ../
+./bin/gocc simd_avx.c   --arch avx2  -O1 --package bitmap -o ../ 
+./bin/gocc simd_neon.c  --arch neon  -O3 --package bitmap -o ../
+./bin/gocc simd_apple.c --arch apple -O3 --package bitmap -o ../
