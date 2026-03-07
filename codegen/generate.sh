bfabcc8285c26d0332fdd45f0221b24f5e688467
Roman Atachiants <roman.atachiants@gmail.com>
2026-03-07 17:18:04 +0400

    Update dependencies (#45)

--- codegen/generate.sh
+++ codegen/generate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./bin/gocc simd_avx.c    --arch avx2   -O1 --package bitmap -o ../ 
-./bin/gocc simd_avx512.c --arch avx512 -O3 --package bitmap -o ../
-./bin/gocc simd_neon.c   --arch neon   -O3 --package bitmap -o ../
-./bin/gocc simd_apple.c  --arch apple  -O3 --package bitmap -o ../
+# requires gocc: go install github.com/kelindar/gocc/cmd/gocc@latest 
+
+gocc simd_avx.c    --arch avx2   -O1 --package bitmap -o ../ 
+gocc simd_avx512.c --arch avx512 -O3 --package bitmap -o ../
+gocc simd_neon.c   --arch neon   -O3 --package bitmap -o ../
+gocc simd_apple.c  --arch apple  -O3 --package bitmap -o ../
